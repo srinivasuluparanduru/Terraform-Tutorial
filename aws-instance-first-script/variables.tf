f8e98548820b9e264065a809e291b0670e06b314
Srinivasulu Paranduru <srinivasuluparanduru@gmail.com>
2023-04-12 19:44:30 +0100

    Update variables.tf

--- aws-instance-first-script/variables.tf
+++ aws-instance-first-script/variables.tf
@@ -6,7 +6,7 @@ variable "ami_id" {
   type = "map"
 
   default = {
-    eu-west-1    ="ami-09dd5f12915cfb387"
+    eu-west-1    = "ami-09dd5f12915cfb387"
     us-east-1    = "ami-035b3c7efe6d061d5"
   }
 }
